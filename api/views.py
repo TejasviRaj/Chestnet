a1769235aec85ca5037bdfb244a942221ff35725
Tejasvi <tejpant@gmail.com>
2019-06-30 14:10:07 +0545

    ajax transfer done

--- api/views.py
+++ api/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def upload(request):
     context = {}
     return render(request, 'api/index1.html', context)
+
+def api(request):
+    context = {}
+    #return HttpResponse( '<img src = "blob:http://127.0.0.1:8000/a3e935de-231a-4b3a-821b-4bb33a656b60">')
+    #return HttpResponse(request.GET.get("add", ""))
+    a='<img src="'+ request.GET.get("add", "") + '">'
+    return HttpResponse(a)
+
