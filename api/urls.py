7fc40efb6949fb0cb77544a198698ceab0f310e3
Tejasvi <tejpant@gmail.com>
2019-07-02 09:49:51 +0545

    multipart done

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.upload, name='upload'),
-    path('api/',views.api,name='api')
+    path('api/',views.api,name='api'),
+    path('first/', views.first, name='first'),
+    path('second/', views.second, name='second'),
+    path('mult/', views.mult, name='mult')
+
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import base64
+import re
 
 
 def upload(request):
@@ -12,4 +14,36 @@ def api(request):
     #return HttpResponse(request.GET.get("add", ""))
     a='<img src="'+ request.POST.get("add", "") + '">'
     return HttpResponse(a)
+
+def first(request):
+    context = {}
+    return render(request, 'api/first.html', context)
+
+def second(request):
+    context = {}
+    data= request.POST.get("val", "")
+    print(len(data))
+    # data += "=" * ((4 - len(data) % 4) % 4)
+    imgdata = base64.b64decode(data+"===")
+    #print(imgdata)
+    d1=data[:len(data)]
+    d2=data[len(data):]
+    with open("d1.txt", 'w') as f:
+        f.write(d1)
+    with open("d2.txt", 'w') as f:
+        f.write(d2)
+    return HttpResponse(data)
+
+    #return render(request, 'api/second.html', context)
+
+def mult(request):
+    f= request.FILES['file']
+
+    with open('keshav.jpg', 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+    return HttpResponse("lol")
+
+
+
 
